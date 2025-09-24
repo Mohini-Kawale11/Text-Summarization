b8b6aaef716ce5828ad8dba3a98b8a7dc9b71515
Mohini-Kawale11 <mohinikawale11@gmail.com>
2025-09-25 03:03:40 +0530

    second changes

---
+++ app.py
@@ -0,0 +1,9 @@
+import nltk
+
+# Download all required NLTK models and corpora
+nltk.download('punkt')                       # Sentence & word tokenizer
+nltk.download('stopwords')                   # Common stop words
+nltk.download('averaged_perceptron_tagger')  # POS tagger
+nltk.download('maxent_ne_chunker')           # Named Entity chunker
+nltk.download('words')                       # Words corpus for NE chunker
+nltk.download('maxent_ne_chunker_tab')       # NEW: Fix for your exact error
