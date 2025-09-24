69272c50825208e114b945f41272287d90ee3271
Mohini-Kawale11 <mohinikawale11@gmail.com>
2025-09-25 03:14:44 +0530

    added mohini comment

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ nltk.download('averaged_perceptron_tagger')  # POS tagger
 nltk.download('maxent_ne_chunker')           # Named Entity chunker
 nltk.download('words')                       # Words corpus for NE chunker
 nltk.download('maxent_ne_chunker_tab')       # NEW: Fix for your exact error
+
+
+# mohini
